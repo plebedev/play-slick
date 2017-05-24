b19efc3696032f5fc7741d383585f055d04fcb93
Peter Lebedev <pete.lebedev@gmail.com>
2017-05-24 00:11:00 -0400

    Add update user, better error handling.

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -5,9 +5,11 @@ import javax.inject.Inject
 import dao.UsersDAO
 import models.User
 import play.api.libs.json.{JsError, Json}
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, Controller, Result}
+import play.api.Logger
 
-import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 /**
   * The controller to handle CRUD operations for [[models.User]].
@@ -22,13 +24,48 @@ class UserController @Inject() (
   def create = Action.async(parse.json) { implicit request =>
     import scala.concurrent.ExecutionContext.Implicits.global
 
-    request.body.validate[User].map{
+    request.body.validate[User].map {
       case (user) => {
-        val result = usersDao.insert(User(user.firstName, user.lastName, user.emailAddress, user.password))
+        val result = usersDao.persist(User(user.firstName, user.lastName, user.emailAddress, user.password))
         result.map(user => Ok(Json.toJson(user)))
+      }.recover {
+        case e => {
+          logger.error("Error inserting user.", e)
+          BadRequest("Error inserting user.")
+        }
       }
     }.recoverTotal{
       e => Future(BadRequest("Detected error:"+ JsError.toJson(e)))
     }
   }
+
+  def update(id: Long) = Action.async(parse.json) { implicit request =>
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    request.body.validate[User].map {
+      case (user) => {
+        val currentUser: Option[User] = Await.result(usersDao.find(id), Duration.Inf)
+
+        val result: Future[Result] = currentUser match {
+          case None => Future(NotFound("User not found."))
+          case Some(u) => {
+              usersDao.persist(User(user.firstName, user.lastName, user.emailAddress, user.password)
+                .copy(id = Option(id), createdAt = u.createdAt))
+                .map(user => Ok(Json.toJson(user)))}
+                .recover {
+                  case e => {
+                    logger.error("Error updating user.", e)
+                    BadRequest("Error updating user.")
+                  }
+                }
+        }
+
+        result
+      }
+    }.recoverTotal{
+      e => Future(BadRequest("Detected error:"+ JsError.toJson(e)))
+    }
+  }
+
+  val logger: Logger = Logger(this.getClass())
 }
--- app/db/EntityManager.scala
+++ app/db/EntityManager.scala
@@ -1,5 +1,6 @@
 package db
 
+import play.api.Logger
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.driver.JdbcProfile
 
@@ -22,9 +23,35 @@ abstract class EntityManager[T <: TableDefinition[E], E <: Entity[E]]
     * @param entity an entity to be inserted
     * @return a future whose value is the ID of the inserted record
     */
-  def insert(entity: E): Future[E] = {
-    val prepersistEntity: E = entity.prePersist
-    db.run((tableQuery returning tableQuery.map(_.id)) += prepersistEntity)
-      .map(res => prepersistEntity.postPersist(res.get))
+  def persist(entity: E): Future[E] = {
+    entity.id match {
+      case None => insert(entity)
+      case Some(_) => update(entity)
+    }
+
   }
+
+  /**
+    * Returns an entity with a given id
+    */
+  def find(id: Long): Future[Option[E]] = {
+    db.run(tableQuery.filter(_.id === id).result.headOption)
+  }
+
+  private def insert(entity: E): Future[E] = {
+    val preinserttEntity: E = entity.prePersist
+
+    logger.error("Insert: createdAt: " + preinserttEntity.createdAt.get.toString)
+    logger.error("Insert: updatedAt: " + preinserttEntity.updatedAt.get.toString)
+    db.run((tableQuery returning tableQuery.map(_.id)) += preinserttEntity)
+      .map(res => preinserttEntity.postPersist(res.get))
+  }
+
+  private def update(entity: E): Future[E] = {
+    val preupdateEntity: E = entity.preUpdate
+    logger.error("Update: updatedAt: " + preupdateEntity.updatedAt.get.toString)
+    db.run(tableQuery.update(preupdateEntity)).map(res => preupdateEntity)
+  }
+
+  val logger: Logger = Logger(this.getClass())
 }
